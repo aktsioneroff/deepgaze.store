db3fbf8d632c5370307ce4d8ab740f4640e04217
aktsioneroff <spishch@yandex.ru>
2026-06-27 19:23:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const AWS = require('aws-sdk');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
